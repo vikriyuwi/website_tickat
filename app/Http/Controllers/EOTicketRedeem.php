30184d916300330f4b26231b4d587dae8aa8441d
novalsh <muhammad.harjana@binus.ac.id>
2023-01-14 13:50:15 +0700

    eo controller

--- app/Http/Controllers/EOTicketRedeem.php
+++ app/Http/Controllers/EOTicketRedeem.php
@@ -3,87 +3,55 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Session;
+use App\Models\TicketRedeem as TRModel;
+use App\Models\Customer as CModel;
+use App\Models\Payment as PModel;
+use App\Models\Ticket as TModel;
+use App\Models\Event as EModel;
+use App\Models\EventOrganizer as EOModel;
 
 class EOTicketRedeem extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         if(!Session::get('Login') || Session::get('LoginRole') != 'EventOrganizer')
         {
             return redirect('/login/event-organizer')->with('status', 'You have to login first!');
         }
+        $redeems = TRModel::all();
+        $events = EModel::where('EventOrganizerId','=',Session::get('LoginId'))->get();
+        $eos = EOModel::all();
+        return view('my-event.ticketreedem.index',['events' => $events,'eos' => $eos,'redeems' => $redeems]);
+    }
+
+    public function create()
+    {
         
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        //
+        
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        
     }
 }
--- resources/views/my-event/ticketreedem/index.blade.php
+++ resources/views/my-event/ticketreedem/index.blade.php
@@ -1,12 +1,97 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+{{-- tetep --}}
+@extends('../../my-event-template')
+
+{{-- tetep --}}
+@section('page-title','Dashboard')
+
+{{-- Sessuain --}}
+@section('title','View ticketreedem')
+
+{{--  --}}
+
+{{--  --}}
+@section('breadcrumb')
+<li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="{{ url('/dashboard') }}">Dashboard</a></li>
+<li class="breadcrumb-item text-sm text-dark active" aria-current="page">Ticket Readem</li>
+@endsection
+
+@section('main-content')
+<section>
+    <div class="container">
+        @if(session()->has('status'))
+        <div class="row">
+            <div class="col-12">
+                <div class="alert alert-success py-3" role="alert">
+                    {{ session('status') }}
+                </div>
+            </div>
+        </div>
+        @endif
+        <div class="row">
+            <div class="col-12">
+                <div class="card mb-4">
+                    <div class="card-header pb-0">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h6>Ticket Readem Data</h6>
+                            </div>
+                            <div class="col-md-6 text-end">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body px-0 pt-0 pb-2">
+                        <div class="table-responsive p-0">
+                            <table class="table align-items-center mb-0">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">ID</th>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Customer</th>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Ticket</th>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Status</th>
+                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($redeems as $data)
+                                    <tr>
+                                        <td class="text-center">
+                                            <p class="text-xs font-weight-bold mb-0">{{ $loop->iteration }}</p>
+                                        </td>
+                                        <td>
+                                            <div class="d-flex px-2 py-1">
+                                                <div>
+                                                    <img src="{{ url('/assets/img/team-1.jpg') }}" class="avatar avatar-sm me-3" alt="user1">
+                                                </div>
+                                                <div class="d-flex flex-column justify-content-center">
+                                                    <h6 class="mb-0 text-sm">{{ $data->customer->CustomerName }}</h6>
+                                                    <p class="text-xs text-secondary mb-0">{{ $data->RedeemCode }}</p>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="d-flex px-2 py-1">
+                                                <div class="d-flex flex-column justify-content-center">
+                                                    <h6 class="mb-0 text-sm">{{ $data->ticket->event->EventName }}</h6>
+                                                    <span class="badge badge-sm bg-{{ $data->ticket->TicketColor }}">{{ $data->ticket->TicketName }}</span>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <span class="badge badge-sm bg-gradient-{{ $data->Status == 'READY' ? 'success' : 'danger' }}">{{ $data->Status }}</span>
+                                        </td>
+                                        <td>
+                                            <a href="{{ url('my-event/redeem/'.$data->TicketRedeemId) }}" class="btn btn-sm btn-primary px-3 mb-0"><i class="fas fa-eye"></i></a>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
